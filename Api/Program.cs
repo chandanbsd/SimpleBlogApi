eb0b7f80aa614892a4737f71bb256e6254f35b60
Chandan Bangalore Shankar Denanath <49138440+chandanbsd@users.noreply.github.com>
2024-12-05 02:02:30 -0500

    Add swagger to all env

--- Api/Program.cs
+++ Api/Program.cs
@@ -20,11 +20,9 @@ var app = builder.Build();
 var cosmosDbService = app.Services.GetRequiredService<ICosmosDbService>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
